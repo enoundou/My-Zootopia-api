8fa477b03db3a8005259d1746c4a6f79f926aa60
Elianne Noundou <nouneliane@gmail.com>
2026-04-15 16:32:17 +0200

    new project architecture

---
+++ animals_web_generator.py
@@ -0,0 +1,67 @@
+
+import data_fetcher
+
+
+def serialize_animal(animal_obj) :
+    """
+    Get the HTML information for an “animal” object
+    :param animal_obj:
+    :return: HTML string of an “animal” object
+    """
+    output_obj =''
+    output_obj += '<li class="cards__item">\n'
+    output_obj += f"<div class='card__title'>{animal_obj['name']}</div>\n"
+    output_obj += '<div class="card__text">'
+    output_obj += '<ul>'
+    if 'characteristics' in animal_obj and 'diet' in animal_obj['characteristics']:
+        output_obj += f"<li><strong>Diet:</strong> {animal_obj['characteristics']['diet']}</li>\n"
+    if 'taxonomy' in animal_obj :
+        if 'kingdom' in animal_obj['taxonomy']:
+            output_obj += f"<li><strong>Kingdom:</strong> {animal_obj['taxonomy']['kingdom']}</li>\n"
+        if 'scientific_name' in animal_obj['taxonomy']:
+            output_obj += (f"<li><strong>Scientific name:</strong> "
+                           f"{animal_obj['taxonomy']['scientific_name']}</li>\n")
+    if 'locations' in animal_obj and len(animal_obj['locations']) > 0:
+        output_obj += f"<li><strong>Location:</strong> {animal_obj['locations'][0]}</li>\n"
+    if 'characteristics' in animal_obj :
+        if'type' in animal_obj['characteristics']:
+            output_obj += f"<li><strong>Type:</strong> {animal_obj['characteristics']['type']}</li>\n"
+        if'group' in animal_obj['characteristics']:
+            output_obj += f"<li><strong>Group:</strong> {animal_obj['characteristics']['group']}</li>\n"
+        if'skin_type' in animal_obj['characteristics']:
+            output_obj += f"<li><strong>Skin type:</strong> {animal_obj['characteristics']['skin_type']}</li>\n"
+    output_obj += '</ul>'
+    output_obj += '</div>'
+    output_obj += '</li>'
+
+    return output_obj
+
+
+def main():
+    animal = input("Please enter an animal: ")
+
+    data = data_fetcher.fetch_data(animal)
+    if not data:
+        output = f"<h2>The animal '{animal}' doesn't exist.</h2>"
+    elif len(data) > 0 :
+        output = ''
+        for animal_obj in data:
+            output += serialize_animal(animal_obj)
+    else:
+        output = f"<h2>Error</h2>"
+
+    # open and read file
+    with open("animals_template.html", "r") as file:
+        content = file.read()
+
+    # replace __REPLACE_ANIMALS_INFO__
+    content = content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    # write to a new file instead of overwriting template
+    with open("animals.html", "w") as file:
+        file.write(content)
+
+    print("Website was successfully generated to the file animals.html.")
+
+if __name__ == '__main__':
+    main()
---
+++ data_fetcher.py
@@ -0,0 +1,41 @@
+import requests
+
+
+API_KEY="BiZHTgjf2TWOZxWXmE8NbBQkXtlGhcGPXeOfoQH8"
+URL_ANIMALS = f"https://api.api-ninjas.com/v1/animals"
+def load_data(animal):
+    """
+    load animals data from api
+    :param animal: animal to load
+    :return: list of animals data
+    """
+
+
+def fetch_data(animal_name):
+    """
+    Fetches the animals data for the animal 'animal_name'.
+    Returns: a list of animals, each animal is a dictionary:
+    {
+    'name': ...,
+    'taxonomy': {
+      ...
+    },
+    'locations': [
+      ...
+    ],
+    'characteristics': {
+      ...
+    }
+    },
+    """
+    headers  = {"X-Api-Key": API_KEY}
+    params = {"name": animal_name}
+    try:
+        response = requests.get(URL_ANIMALS, headers=headers, params=params)
+        status_code = response.status_code
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
